2e4c68c303bdd1796d8c231ae6e8cb04f3218313
Nick Melkadze <25963683+melkadze@users.noreply.github.com>
2020-02-06 14:46:59 -0500

    import my repo and demo node repo

---
+++ src/models/user.js
@@ -0,0 +1,94 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 6,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error('Password cannot "password" ');
+      }
+    }
+  },
+  graduated: {
+    type: Boolean,
+    default: false
+  },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true
+      }
+    }
+  ],
+  profilePic: {
+    type: Buffer
+  }
+});
+
+userSchema.virtual("reviews", {
+  ref: "Review",
+  localField: "_id",
+  foreignField: "owner"
+})
+
+userSchema.methods.toJSON = function() {
+  const user = this;
+  const userObject = user.toObject();
+  delete userObject.tokens;
+  delete userObject.password;
+  return userObject;
+}
+
+userSchema.methods.generateToken = async function() {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, "obeysudo");
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+  return token;
+};
+userSchema.pre("save", async function(next) {
+  const user = this;
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 7);
+  }
+  next();
+});
+//when we send a post or patch request, then bcrypt will run BEFORE the user password is saved to the mongo object.
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error("unable to login");
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error("incorrect password");
+  }
+  return user;
+};
+const User = mongoose.model("User", userSchema);
+module.exports = User;
