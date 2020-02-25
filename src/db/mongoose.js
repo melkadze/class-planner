c1aeed3214b097c194c1a85de1f9755912b40961
melkadze <25963683+melkadze@users.noreply.github.com>
2020-02-25 15:40:17 -0500

    Update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const env = require("../env")
 
 mongoose.connect(
-  `mongodb+srv://application_master:${env.mongoosePassword}@primary-qvy3u.mongodb.net/test?retryWrites=true&w=majority`,
+  `mongodb+srv://application_master:${env.mongoosePassword}@primary-qvy3u.mongodb.net
+  /test?retryWrites=true&w=majority`,
   {
     useNewUrlParser: true,
     useCreateIndex: true,
