29675db7163e71cb90e5a0295bfbc525e140133b
Nick Melkadze <25963683+melkadze@users.noreply.github.com>
2020-02-27 01:35:37 -0500

    Delete env.js

--- src/env.js
+++
@@ -1,6 +0,0 @@
-//mongoose
-exports.mongoosePassword = 'obeysudo_class-planner'
-
-//oauth
-exports.oAuthID = '159312246325-bsaph1nbgvfjtkj7um10p9chokpq10sj.apps.googleusercontent.com'
-exports.oAuthSecret = 'DRTT5kaigPItWm6XPr9c7-ju'
