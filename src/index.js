07feaeb1c7f80afd9087985b5887e2d648c8f45d
melkadze <25963683+melkadze@users.noreply.github.com>
2020-02-27 23:10:12 -0500

    clean up

--- src/index.js
+++ src/index.js
@@ -1,45 +1,39 @@
+//visit this project at github.com/melkadze/class-planner
+
+//framework dependencies
+const express = require("express");
+const path = require('path')
+const app = express();
+require("./db/mongoose"); //ensures mongoose runs
+
+//framework setup
+app.set('views', path.join(__dirname, './views')); //redefine views path
+app.set('view engine', 'ejs'); //use ejs
+app.use(express.json());
+
+//other dependencies
+const passport = require('passport')
+const cookieSession = require('cookie-session')
+const passportSetup = require('./config/passport-setup') //this is necessary
+const env = require('./env')
 //import * as sampledb from "./sampledb.json"
 
-const express = require("express");
-require("./db/mongoose"); //ensures mongoose runs and connects to our database
-const app = express();
-const path = require('path')
-
-app.set('views', path.join(__dirname, './views'));
-app.set('view engine', 'ejs');
-
-const passportSetup = require('./config/passport-setup')
-const passport = require('passport')
-
-const cookieSession = require('cookie-session')
-const env = require('./env')
-
-const movieRouter = require("./routers/movies");
-const userRouter = require("./routers/user");
-const reviewRouter = require("./routers/review");
-app.use(express.json());
-app.use(movieRouter);
-app.use(userRouter);
-app.use(reviewRouter);
-
+//setup cookies for login storage
 app.use(cookieSession({
-  maxAge: 24 * 60 * 60 * 1000, //in ms
+  maxAge: 24 * 60 * 60 * 1000, //one day, in ms
   keys: [env.cookieKey]
 }))
 
-// init passport
+//initialize passport
 app.use(passport.initialize());
 app.use(passport.session());
 
-//const moment = require('moment')
-
-app.listen(3000, () => {
-  console.log("Server up on 3000");
-});
-
+//router dependencies
 const authRouter = require('./routers/auth')
-app.use('/auth', authRouter)
 const profileRouter = require('./routers/profile')
+
+//router setup
+app.use('/auth', authRouter)
 app.use('/profile', profileRouter)
 
 //put this in routes later
@@ -47,55 +41,7 @@ app.get('/', (req, res) => {
   res.render('home', {user: req.user});
 })
 
-//bcrypt hashing will occur as middleware during requests
-
-/* const bcrypt = require("bcryptjs");
-const testFunction = async () => {
-  const password = "obeysudo";
-  const hashedPassword = await bcrypt.hash(password, 10);
-  console.log(password);
-  console.log(hashedPassword);
-
-  const isMatch = await bcrypt.compare("obeysudo", hashedPassword);
-  console.log(isMatch);
-};
-
-testFunction(); */
-
-/* const jwt = require("jsonwebtoken");
-
-const testFunction = async () => {
-  const token = jwt.sign({ _id: "5e1f3f813f07551900b85ea5" }, "obeysudo", {
-    expiresIn: "7 days"
-  });
-  console.log(token);
-  const data = jwt.verify(token, "obeysudo");
-  console.log(data);
-};
-
-testFunction(); */
-
-/*
-const Review = require("./models/review");
-const User = require("./models/user");
-
-const main = async() => {
-  const user = await User.findById("5e1f407d6fb8e37a3cdc391f");
-  await user.populate("reviews").execPopulate();
-  console.log(user.reviews);
-}
-
-main();
-*/
-
-
-/*
-function howLongUntil() {
-  const currentTime = moment().format("H:mm")
-} 
-
-let i = 6
-i--
-console.log(Number((sampledb.sample[i].period)))
-console.log((sampledb.sample[i].name))
-*/
+//finally, open the server
+app.listen(3000, () => {
+  console.log("Server up on 3000");
+});
