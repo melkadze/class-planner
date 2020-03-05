d31ec0d2f4816d313b2319f28ce6fdea1d9c844c
vbalte <54951038+vbalte@users.noreply.github.com>
2020-03-05 11:41:04 -0500

    okk

---
+++ app.js
@@ -0,0 +1,14 @@
+///do not distribute
+
+module.exports = {
+    mongooseString: 'mongodb+srv://application_master:class_planner-obeysudo-2@primary-qvy3u.mongodb.net/test?retryWrites=true&w=majority',
+
+    oAuthID: '159312246325-bsaph1nbgvfjtkj7um10p9chokpq10sj.apps.googleusercontent.com',
+    oAuthSecret: 'dS2zIAUKB1Od-GcU4QJGHrA5',
+
+    cookieKey: 'verySECUREkeyTHATcannotBEhacked',
+
+    email: 'classplanner@melkadze.com',
+
+    port: 3000
+}
