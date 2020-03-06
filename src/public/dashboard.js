044dea161d5c4c5f595e668e7fde548fce1df4f9
vbalte <54951038+vbalte@users.noreply.github.com>
2020-03-06 11:43:53 -0500

    okk

--- src/public/dashboard.js
+++ src/public/dashboard.js
@@ -1,3 +1,12 @@
 const moment = require("moment");
 
-console.log(moment());
+console.log(m());
+
+let m = moment();
+
+console.log(`toString() => ${m.toString()}`); // local time zone
+console.log(`toISOString() => ${m.toISOString()}`);
+
+//create from ISO 8601 String
+
+m = moment("2020-03-06T11:31:00");
