8853f34b16f2010383a4e752a33bb383481ae319
melkadze <25963683+melkadze@users.noreply.github.com>
2020-03-22 23:55:04 -0400

    frontend demo fully operational

--- src/public/timing.js
+++ src/public/timing.js
@@ -23,6 +23,7 @@ function convertToTime(hour, minute, isPM) {
 }
 
 function sendPOST(url, content) {
+    //no longer necessary, rec'd to rem
     const xhttp = new XMLHttpRequest();
     xhttp.open('POST', url, true);
     xhttp.setRequestHeader('Content-Type', 'application/json')
@@ -42,30 +43,59 @@ document.getElementById('periodInputButton').onclick = function () {
     const convertedTimeStart = convertToTime(hourStart, minuteStart, apmStart)
     const convertedTimeEnd = convertToTime(hourEnd, minuteEnd, apmEnd)
     
-    sendPOST("/schedule/period/upload", `{"forSchedule": "${forSchedule}", "period": "${period}", "timeStart": "${convertedTimeStart}", "timeEnd": "${convertedTimeEnd}"}`)
+    //sendPOST("/schedule/period/upload", `{"forSchedule": "${forSchedule}", "period": "${period}", "timeStart": "${convertedTimeStart}", "timeEnd": "${convertedTimeEnd}"}`)
+    
+    axios.post(`/schedule/period/upload`, {
+        forSchedule: forSchedule,
+        period: period,
+        timeStart: convertedTimeStart,
+        timeEnd: convertedTimeEnd
+    })
+    .then (function (response) {
+        document.getElementById('periodError').innerHTML = ''
+        document.getElementById('periodSuccess').innerHTML = 'Period send OK!'
+    })
+    .catch(function (error) {
+        document.getElementById('periodSuccess').innerHTML = ''
+        document.getElementById('periodError').innerHTML = error
+    })
 }
 
 document.getElementById('scheduleRetrieveButton').onclick = function () {
-    const schedule = document.getElementById('scheduleRetrieveButton').value
+    const schedule = document.getElementById('scheduleRetrieveSchedule').value
     
-    axios.get('/schedule/NTest')
+    axios.get(`/schedule/${schedule}`)
     .then (function (response) {
         console.log(response.data)
+        
+        document.getElementById('scheduleGetError').innerHTML = ''
+        document.getElementById('scheduleGetSuccess').innerHTML = 'Schedule get OK!'
     
-        for (i = 1; i < 10; i++) {
+        for (i = 0; i < response.data.length; i++) {
             document.getElementById(`periodDisplayPeriod${i}`).innerHTML = response.data[i].period
             document.getElementById(`periodDisplayTimeStart${i}`).innerHTML = luxon.DateTime.fromISO(response.data[i].timeStart).toFormat('h:mm a')
             document.getElementById(`periodDisplayEnd${i}`).innerHTML = luxon.DateTime.fromISO(response.data[i].timeEnd).toFormat('h:mm a')
         }
     })
     .catch(function (error) {
-        console.log(`ERROR: ${error}`)
+        document.getElementById('scheduleGetSuccess').innerHTML = ''
+        document.getElementById('scheduleGetError').innerHTML = error
+        console.log(error)
     })
-    .finally(function () {
-        console.log('OK')
+}
+
+document.getElementById('scheduleInputButton').onclick = function () {
+    axios.post(`/schedule/upload`, {
+        name: document.getElementById('scheduleInputSchedule').value
     })
-    
-    sendPOST("/schedule/period/upload", `{"forSchedule": "${forSchedule}", "period": "${period}", "timeStart": "${convertedTimeStart}", "timeEnd": "${convertedTimeEnd}"}`)
+    .then (function (response) {
+        document.getElementById('scheduleError').innerHTML = ''
+        document.getElementById('scheduleSuccess').innerHTML = 'Schedule send OK!'
+    })
+    .catch(function (error) {
+        document.getElementById('scheduleSuccess').innerHTML = ''
+        document.getElementById('scheduleError').innerHTML = error
+    })
 }
 
 
