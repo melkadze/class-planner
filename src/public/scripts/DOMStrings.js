671f43dd789be5c6f430e76fd4fdef07d48a0006
Edward Ng <edward.ng@sitechhs.com>
2020-04-18 23:40:55 -0400

    Dashboard
    General:
    Created "front-dashboard.ejs" view with minimal functionality.
    Dashboard includes template for navigation bar, greeting module, calendar module, alarms and timers module, courses module, and tasks module.
    Calendar and clock components are functional.
    Buttons and navigation are not functional.
    Tested for viewport widths between 1280 to 1920.
    
    Expanded:
    Created various icons and images in "src/public/assets/svg".
    Created SCSS base component (effects).
    Created a "time.js" module that includes methods for determining and displaying the month, days, year, hours, minutes, and seconds.
    Created a "DOMStrings.js" module that includes an object storing DOMStrings.
    Created an "index.js" script that runs methods from "time.js".
    Modified "package.json" to include development script to watch for SCSS changes.
    Modified SCSS base components (color, global, grid, and typography).
    Modified SCSS net components (dashboard and navbar).
    Moved dashboard from "app.ejs" to "front-dashboard.ejs".

---
+++ src/public/scripts/DOMStrings.js
@@ -0,0 +1,9 @@
+const DOMStrings = {
+  calendar: document.getElementById("calendar"),
+  calendar__headline: document.getElementById("calendar__headline"),
+  clock__hour: document.getElementById("clock__hour"),
+  clock__minute: document.getElementById("clock__minute"),
+  clock__second: document.getElementById("clock__second"),
+  alarms__digital: document.getElementById("alarms__digital"),
+};
+export { DOMStrings };
---
+++ src/public/scripts/index.js
@@ -0,0 +1,6 @@
+import { time, displayTime } from "./time.js";
+
+displayTime.displayCurrentMonthAndYear();
+displayTime.displayDays(time.getCurrentMonth(), time.getCurrentYear());
+displayTime.displayAnalogTime();
+displayTime.displayDigitalTime();
