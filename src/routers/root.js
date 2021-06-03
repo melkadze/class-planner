74c33abc35f8a86a37c3a3ae3ef9b01bc799a148
Nick Melkadze <25963683+melkadze@users.noreply.github.com>
2021-06-02 22:05:30 -0400

    add certbot challenges

--- src/routers/root.js
+++ src/routers/root.js
@@ -18,5 +18,19 @@ router.get("/.well-known/acme-challenge/6n5aqF9Y_otZRV_5xZXzIFL96EFsxqgqjKVV1Krh
 		functions.error(res, 500, err)
 	}
 })
+router.get("/.well-known/acme-challenge/nzQENyRpkRAH6H3ebOoopMo5VED1KJUDpbcN_zWX7fY", (req, res) => {
+	try{
+		res.send("nzQENyRpkRAH6H3ebOoopMo5VED1KJUDpbcN_zWX7fY.-AcrsV2YDI6oOMWRahbO1pcD_tYXvaiPWZRNEb5EKYA")
+	} catch(err) {
+		functions.error(res, 500, err)
+	}
+})
+router.get("/.well-known/acme-challenge/rUzFGnjRj6s2gx1fERIxoYFnKveVSodDJf49jhwsbdY", (req, res) => {
+	try{
+		res.send("rUzFGnjRj6s2gx1fERIxoYFnKveVSodDJf49jhwsbdY.-AcrsV2YDI6oOMWRahbO1pcD_tYXvaiPWZRNEb5EKYA)
+	} catch(err) {
+		functions.error(res, 500, err)
+	}
+})
 
 module.exports = router
